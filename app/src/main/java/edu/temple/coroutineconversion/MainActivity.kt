d7a127faa9f61b507efe2841cc8da2ba36022786
aliasgersg <alighotawala02@gmail.com>
2023-11-02 11:47:42 -0400

    In this commit, I replaced my threads with coroutines  and setting my dispatcher to default instead of main.

--- app/src/main/java/edu/temple/coroutineconversion/MainActivity.kt
+++ app/src/main/java/edu/temple/coroutineconversion/MainActivity.kt
@@ -2,21 +2,15 @@ package edu.temple.coroutineconversion
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import android.widget.Button
 import android.widget.ImageView
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
 
-    //TODO (Refactor to replace Thread code with coroutines)
-
     lateinit var cakeImageView: ImageView
 
-    val handler = Handler(Looper.getMainLooper(), Handler.Callback {
-        cakeImageView.alpha = it.what / 100f
-        true
-    })
+    private val coroutineScope = CoroutineScope(Dispatchers.Default)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,13 +18,13 @@ class MainActivity : AppCompatActivity() {
 
         cakeImageView = findViewById(R.id.imageView)
 
-        findViewById<Button>(R.id.revealButton).setOnClickListener{
-            Thread{
+        findViewById<Button>(R.id.revealButton).setOnClickListener {
+            coroutineScope.launch {
                 repeat(100) {
-                    handler.sendEmptyMessage(it)
-                    Thread.sleep(40)
+                    cakeImageView.alpha = it / 100f
+                    delay(40)
                 }
-            }.start()
+            }
         }
     }
 }
